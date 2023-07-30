19eefb6ea565d66d4d54a29304565c65e9abf745
Ketok <ketok4321@gmail.com>
2023-07-30 12:59:17 +0200

    A little refactor

--- src/main.py
+++ src/main.py
@@ -1,28 +1,36 @@
 import sys
 import gi
+import threading
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 gi.require_foreign("cairo")
 
-from gi.repository import Gio, Gtk, Adw
+from gi.repository import GLib, Gio, Gtk, Adw
 
 from .window import SpeedtestWindow
 from .gauge import Gauge # This class isn't used there but it the widget needs to be registered 
+from .speedtest import Speedtest
 
 class SpeedtestApplication(Adw.Application):
+    win = None
+    speedtest = Speedtest(secure=True)
+    test_thread = None
+
     def __init__(self):
-        super().__init__(application_id="xyz.ketok.Speedtest",
-                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+        super().__init__(application_id="xyz.ketok.Speedtest", flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+        
         self.create_action("quit", lambda *_: self.quit(), ["<primary>q"])
         self.create_action("about", self.on_about_action)
         self.create_action("preferences", self.on_preferences_action)
+        self.create_action("start", self.on_start_action)
+        self.create_action("cancel", self.on_cancel_action)
 
     def do_activate(self):
-        win = self.props.active_window
-        if not win:
-            win = SpeedtestWindow(application=self)
-        win.present()
+        self.win = self.props.active_window
+        if not self.win:
+            self.win = SpeedtestWindow(application=self)
+        self.win.present()
 
     def on_about_action(self, widget, _): #TODO: Credit speedtest-cli and ookla
         about = Adw.AboutWindow(transient_for=self.props.active_window,
@@ -37,13 +45,35 @@ class SpeedtestApplication(Adw.Application):
     def on_preferences_action(self, widget, _):
         print("app.preferences action activated")
 
+    def on_start_action(self, widget, _):
+        self.win.view_switcher.set_visible_child(self.win.test_view)
+
+        test_thread = threading.Thread(target=self.do_start)
+        test_thread.daemon = True
+        test_thread.start()
+    
+    def do_start(self): # TODO: Try except
+        view = self.win.test_view
+        
+        self.speedtest.get_best_server() # This measures ping
+
+        view.ping = str(round(self.speedtest.results.ping)) + "ms"
+        dl = self.speedtest.download(lambda *args, speed=None, end=None, **kwargs: GLib.idle_add(lambda: view.updateDownload(speed)) if end else None)
+        up = self.speedtest.upload(lambda *args, speed=None, end=None, **kwargs: GLib.idle_add(lambda: view.updateUpload(speed)) if end else None)
+        GLib.idle_add(lambda: view.updateDownload(dl))
+        GLib.idle_add(lambda: view.updateUpload(up))
+    
+    def on_cancel_action(self, widget, _):
+        # TODO: Abort the thread
+
+        self.win.view_switcher.set_visible_child(self.win.start_view)
+
     def create_action(self, name, callback, shortcuts=None):
         action = Gio.SimpleAction.new(name, None)
         action.connect("activate", callback)
         self.add_action(action)
         if shortcuts:
             self.set_accels_for_action(f"app.{name}", shortcuts)
-
 
 def main(version):
     app = SpeedtestApplication()
--- src/window.py
+++ src/window.py
@@ -5,8 +5,6 @@ from gi.repository import GObject
 from gi.repository import Gtk
 from gi.repository import Adw
 
-from .speedtest import Speedtest
-
 def toMb(speed):
     return round(speed / 1_000_000)
 
@@ -19,34 +17,17 @@ class SpeedtestWindow(Adw.ApplicationWindow):
     start_view = Gtk.Template.Child()
     test_view = Gtk.Template.Child()
 
-    test_thread = None
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        self.get_application().create_action("start", self.on_start_action)
-        self.get_application().create_action("cancel", self.on_cancel_action)
-
-    def on_start_action(self, widget, _):
-        self.view_switcher.set_visible_child(self.test_view)
-
-        test_thread = threading.Thread(target=self.test_view.start)
-        test_thread.daemon = True
-        test_thread.start()
-
-    
-    def on_cancel_action(self, widget, _):
-        # TODO: Abort the thread
-
-        self.view_switcher.set_visible_child(self.start_view)
 
 @Gtk.Template(resource_path="/xyz/ketok/Speedtest/ui/views/start.ui")
 class StartView(Gtk.Box):
     __gtype_name__ = "StartView"
 
+    server_selector = Gtk.Template.Child()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
 
 @Gtk.Template(resource_path="/xyz/ketok/Speedtest/ui/views/test.ui")
 class TestView(Gtk.Box):
@@ -58,25 +39,14 @@ class TestView(Gtk.Box):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
-    def start(self): # TODO: Try except
-        speedtest = Speedtest(secure=True)
-        speedtest.get_best_server() # This measures ping
 
-        self.ping = str(round(speedtest.results.ping)) + "ms"
-        dl = speedtest.download(self.callback(self.download))
-        up = speedtest.upload(self.callback(self.upload))
-        self.callback(self.download)(0, 0, end=True, speed=dl)
-        self.callback(self.upload)(0, 0, end=True, speed=up)
+    def updateGauge(self, object, speed):
+        speedMb = toMb(speed)
+        object.label = str(speedMb) + "Mbps"
+        object.fill = min(toMb(speed) / 100, 1.0)
 
-    def callback(self, object):
-        def result(i, request_count, speed=None, start=False, end=False):
-            if end:
-                speedMb = toMb(speed)
+    def updateDownload(self, speed):
+        self.updateGauge(self.download, speed)
 
-                def why_the_fuck_doesnt_python_support_multiline_lambdas():
-                    object.label = str(speedMb) + "Mbps"
-                    object.fill = min(toMb(speed) / 100, 1.0)
-
-                GLib.idle_add(why_the_fuck_doesnt_python_support_multiline_lambdas)
-        return result
+    def updateUpload(self, speed):
+        self.updateGauge(self.upload, speed)
