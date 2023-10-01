685f3426e8a02566d027c8e16ec0271bdbb02b3e
Ketok <ketok4321@gmail.com>
2023-10-01 19:51:39 +0200

    Stuff

--- src/window.py
+++ src/window.py
@@ -1,5 +1,41 @@
 from gi.repository import GObject, Gio, Gtk, Adw
 
+# I have shamelessly stole it from https://gitlab.gnome.org/GNOME/pygobject/-/issues/98
+def bind_with_mapping(self, key, widget, prop, flags, key_to_prop, prop_to_key):
+    """
+    Recreate g_settings_bind_with_mapping from scratch.
+    This method was shamelessly stolen from Robert Park's
+    gottengeography who shamelessly stolen it from John Stowers'
+    gnome-tweak-tool on May 14, 2012.
+    """
+    self._ignore_key_changed = False
+
+    def key_changed(settings, key):
+        if self._ignore_key_changed:
+            return
+        self._ignore_prop_changed = True
+        widget.set_property(prop, key_to_prop(self[key]))
+        self._ignore_prop_changed = False
+
+    def prop_changed(widget, param):
+        if self._ignore_prop_changed:
+            return
+        self._ignore_key_changed = True
+        self[key] = prop_to_key(widget.get_property(prop))
+        self._ignore_key_changed = False
+
+    if not (flags & (Gio.SettingsBindFlags.SET | Gio.SettingsBindFlags.GET)): # ie Gio.SettingsBindFlags.DEFAULT
+       flags |= Gio.SettingsBindFlags.SET | Gio.SettingsBindFlags.GET
+
+    if flags & Gio.SettingsBindFlags.GET:
+        key_changed(self, key)
+        if not (flags & Gio.SettingsBindFlags.GET_NO_CHANGES):
+            self.connect('changed::' + key, key_changed)
+    if flags & Gio.SettingsBindFlags.SET:
+        widget.connect('notify::' + prop, prop_changed)
+    if not (flags & Gio.SettingsBindFlags.NO_SENSITIVITY):
+        self.bind_writable(key, widget, "sensitive", False)
+
 @Gtk.Template(resource_path="/xyz/ketok/Speedtest/ui/window.ui")
 class SpeedtestWindow(Adw.ApplicationWindow):
     __gtype_name__ = "SpeedtestWindow"
@@ -29,14 +65,20 @@ class SpeedtestWindow(Adw.ApplicationWindow):
 class SpeedtestPreferencesWindow(Adw.PreferencesWindow):
     __gtype_name__ = "SpeedtestPreferencesWindow"
 
-    gauge_scale = Gtk.Template.Child() #TODO: Use Adw.ComboRow.expression?
+    gauge_scale = Gtk.Template.Child()
     backend = Gtk.Template.Child()
+
+    SCALES = [100, 1000]
+    BACKENDS = ["librespeed.org", "speedtest.net"]
 
     def __init__(self, settings, **kwargs):
         super().__init__(**kwargs)
 
-        settings.bind("gauge-scale", self.gauge_scale, "selected", Gio.SettingsBindFlags.DEFAULT)
-        settings.bind("backend", self.backend, "selected", Gio.SettingsBindFlags.DEFAULT)
+        self.gauge_scale.set_model(Gtk.StringList.new(list(map(lambda s: f"{s}Mbps", self.SCALES))))
+        self.backend.set_model(Gtk.StringList.new(self.BACKENDS))
+
+        bind_with_mapping(settings, "gauge-scale", self.gauge_scale, "selected", Gio.SettingsBindFlags.DEFAULT, self.SCALES.index, self.SCALES.__getitem__)
+        bind_with_mapping(settings, "backend", self.backend, "selected", Gio.SettingsBindFlags.DEFAULT, self.BACKENDS.index, self.BACKENDS.__getitem__)
 
 @Gtk.Template(resource_path="/xyz/ketok/Speedtest/ui/views/start.ui")
 class StartView(Gtk.Box):
